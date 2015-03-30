b399dd5621efc0e911b20c4f72d5b5e304d32ce1
jmcourt <J.M.Court@soton.ac.uk>
2015-03-30 11:30:50 +0100

    Removing a print statement left in for debugging...

--- plotdemon.py
+++ plotdemon.py
@@ -383,8 +383,6 @@ while plotopt not in ['quit','exit']:                                     # If t
 
          print 'Clipping...'
 
-         print x1[mint],x1[maxt]
-
          x1=x1[mint:maxt]                                                 # Clip file 1
          y1=y1[mint:maxt]
          ye1=ye1[mint:maxt]
